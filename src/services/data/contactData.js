686a4cd03e074e0201d7d3fe46f96ee61309b9b2
mkothm <mkothman.jr@gmail.com>
2024-10-16 08:05:54 +0100

    added images

--- src/services/data/contactData.js
+++ src/services/data/contactData.js
@@ -1,4 +1,9 @@
 import portrait1 from "../../assets/images/portrait1.jpg";
+import portrait2 from "../../assets/images/portrait2.avif";
+import portrait3 from "../../assets/images/portrait3.avif";
+import portrait4 from "../../assets/images/portrait4.avif";
+import portrait5 from "../../assets/images/portrait5.avif";
+import portrait6 from "../../assets/images/portrait6.avif";
 
 export const contactData = [
   {
@@ -9,17 +14,17 @@ export const contactData = [
   {
     name: "Duane Dean",
     email: "kutt.dartstoll@stdca.to",
-    imageSrc: portrait1,
+    imageSrc: portrait2,
   },
   {
     name: "Jonathan Barner",
     email: "tora_handy@zimbio.biz",
-    imageSrc: portrait1,
+    imageSrc: portrait3,
   },
   {
     name: "Rosie Glover",
     email: "brknows.morselo@homail.com",
-    imageSrc: portrait1,
+    imageSrc: portrait4,
   },
   {
     name: "Patrick Greer",
@@ -29,6 +34,6 @@ export const contactData = [
   {
     name: "Darrell Ortega",
     email: "rahul.shalashaska@ynho.info",
-    imageSrc: portrait1,
+    imageSrc: portrait5,
   },
 ];
--- src/services/data/teamData.js
+++ src/services/data/teamData.js
@@ -1,4 +1,10 @@
 import portrait1 from "../../assets/images/portrait1.jpg";
+import portrait2 from "../../assets/images/portrait2.avif";
+import portrait3 from "../../assets/images/portrait3.avif";
+import portrait4 from "../../assets/images/portrait4.avif";
+import portrait5 from "../../assets/images/portrait5.avif";
+import portrait6 from "../../assets/images/portrait6.avif";
+import portrait7 from "../../assets/images/portrait7.avif";
 
 export const teamData = [
   {
@@ -10,60 +16,60 @@ export const teamData = [
   {
     name: "Joshua Reed",
     role: "UX Designer",
-    imageSrc: portrait1,
+    imageSrc: portrait5,
   },
   {
     name: "Carla Scnhez",
     role: "HR",
-    imageSrc: portrait1,
+    imageSrc: portrait4,
     mail: "lucy@fakemail.net",
   },
   {
     name: "Arlene Mckinney",
     role: "CFO",
-    imageSrc: portrait1,
+    imageSrc: portrait6,
     mail: "xful.example@fakemail.net",
   },
   {
     name: "Marrie Jones",
     role: "Developer",
-    imageSrc: portrait1,
+    imageSrc: portrait5,
     mail: "frrtes@fakemail.net",
   },
   {
     name: "Darlene Robts",
     role: "Designer",
-    imageSrc: portrait1,
+    imageSrc: portrait4,
     mail: "dmfn@fakemail.com",
   },
   {
     name: "Ronald Richards",
     role: "Designer",
-    imageSrc: portrait1,
+    imageSrc: portrait7,
     mail: "musan@fakemail.com",
   },
   {
     name: "Maisha Berry",
     role: "Designer",
-    imageSrc: portrait1,
+    imageSrc: portrait7,
     mail: "isaqq@fakemail.com",
   },
   {
     name: "Elly Tramos",
     role: "Marketing",
-    imageSrc: portrait1,
+    imageSrc: portrait6,
     mail: "werr@iicon.com",
   },
   {
     name: "Howard Atkins",
     role: "CTO",
-    imageSrc: portrait1,
+    imageSrc: portrait4,
     mail: "trropolis@iicon.com",
   },
   {
     name: "Eliza Hamertons",
     role: "CIO",
-    imageSrc: portrait1,
+    imageSrc: portrait2,
     mail: "eliza@iicon.com",
   },
   {
